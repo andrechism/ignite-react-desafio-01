363a2a3de69d63dbaab663ad117acb1837795bcf
André Aragão <andre.correia@agenciaeplus.com.br>
2021-03-11 22:44:31 -0300

    desafio resolvido

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -16,14 +16,34 @@ export function TaskList() {
 
   function handleCreateNewTask() {
     // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    if (!newTaskTitle) return;
+
+    setTasks([...tasks, {
+      id: Math.random(),
+      title: newTaskTitle,
+      isComplete: false
+    }]);
   }
 
   function handleToggleTaskCompletion(id: number) {
     // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    const newTasks = tasks.map(task => {
+      if (task.id !== id) return task;
+      task.isComplete = !task.isComplete;
+      return task;
+    });
+    
+    setTasks(newTasks);
   }
 
   function handleRemoveTask(id: number) {
     // Remova uma task da listagem pelo ID
+    const newTasks = tasks.reduce((acc: Task[], curr) => {
+      if (curr.id === id) return acc;
+      return [...acc, curr];
+    }, []);
+
+    setTasks(newTasks);
   }
 
   return (
